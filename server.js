42b9f281104dc1b5988ea29a28c471f7b2a8142b
tanukile <backpacker320@gmail.com>
2023-10-12 23:57:13 +0800

    auth

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const jsonServer = require('json-server');
+const auth = require('json-server-auth');
+
 const apiServer = jsonServer.create();
 const apiRouter = jsonServer.router('mock/db.json');
 const middlewares = jsonServer.defaults();
+
 apiServer.use(middlewares);
+apiServer.use(auth);
 apiServer.use(apiRouter);
 apiServer.listen(3000, () => {
   console.log('JSON Server is running');
